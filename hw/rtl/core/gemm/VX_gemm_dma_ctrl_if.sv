54475c1840beb1a32d22157d9f855f777ae0f940
gaeunchae <gaeunchae1201@gmail.com>
2026-02-10 16:55:48 +0900

    Add M_tot, N_tot, K_tot, wid for entry allocation

--- hw/rtl/core/gemm/VX_gemm_dma_ctrl_if.sv
+++ hw/rtl/core/gemm/VX_gemm_dma_ctrl_if.sv
@@ -6,14 +6,21 @@ interface VX_gemm_dma_ctrl_if import VX_gpu_pkg::*; ();
   logic idle;
   logic done;
   gemm_unified_cmd_t cmd;
+  
+  logic [31:0] M_tot;
+  logic [31:0] N_tot;
+  logic [31:0] K_tot;
+
+  // 이 DMA 커맨드 스트림의 소유자(워프 ID)
+  logic [31:0] wid;     // = warp_id
 
   modport master (
-    output start, cmd,
+    output start, cmd, M_tot, N_tot, K_tot, wid,
     input idle, done
   );
 
   modport slave (
-    input start, cmd,
+    input start, cmd, M_tot, N_tot, K_tot, wid,
     output idle, done
   );
   
