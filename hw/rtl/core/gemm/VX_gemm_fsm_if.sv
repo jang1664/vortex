1be3e41cc82937cabbab2d6103325a3c7e25f3f0
gaeunchae <gaeunchae1201@gmail.com>
2026-02-12 17:24:43 +0900

    Add qblk interface to gemm_fsm

--- hw/rtl/core/gemm/VX_gemm_fsm_if.sv
+++ hw/rtl/core/gemm/VX_gemm_fsm_if.sv
@@ -19,6 +19,7 @@ interface VX_gemm_fsm_if import VX_gpu_pkg::*; ();
   logic [31:0] M_tot;
   logic [31:0] N_tot;
   logic [31:0] K_tot;
+  logic [31:0] qblk_tot;
 
   // 이 DMA 커맨드 스트림의 소유자(워프 ID)
   logic [31:0] wid;     // = warp_id
@@ -31,6 +32,7 @@ interface VX_gemm_fsm_if import VX_gpu_pkg::*; ();
     output M_tot,
     output N_tot,
     output K_tot,
+    output qblk_tot,
     output wid,
     input  flag
   );
@@ -40,6 +42,7 @@ interface VX_gemm_fsm_if import VX_gpu_pkg::*; ();
     input  M_tot,
     input  N_tot,
     input  K_tot,
+    input  qblk_tot,
     input  wid,
     output flag
   );
