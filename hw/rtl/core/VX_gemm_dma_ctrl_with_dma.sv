a4d83979b4cd781d7caa973464712f241ef299a4
gaeunchae <gaeunchae1201@gmail.com>
2026-02-17 21:49:15 +0900

    Instantiate VX_gemm_dma_ctrl with DMA_CFG_BASE_ADDR parameter and add VX_dma_node

--- hw/rtl/core/VX_gemm_dma_ctrl_with_dma.sv
+++ hw/rtl/core/VX_gemm_dma_ctrl_with_dma.sv
@@ -15,11 +15,11 @@ module VX_gemm_dma_ctrl_with_dma import VX_gpu_pkg::*; #(
   // ------------------------------------------------------------
   // Descriptor layout
   // ------------------------------------------------------------
-  localparam int NUM_REGS     = 16;
+  localparam int NUM_REGS     = `DMA_CFG_REG_NUM;
   localparam int REGS_DW      = 32;
   localparam int NDIM         = 3;
   localparam int NUM_ENTRIES  = 4;
-  localparam int ENTRYID_W    = 4;
+  localparam int ENTRYID_W    = `LOG2UP(NUM_ENTRIES);
 
   localparam int CFG_NUM_LANES = `NUM_LSU_LANES;
   localparam int CFG_DATA_SIZE = LSU_WORD_SIZE; // bytes
@@ -30,20 +30,8 @@ module VX_gemm_dma_ctrl_with_dma import VX_gpu_pkg::*; #(
     .TAG_WIDTH(45)
   ) dma_if[1]();
 
-  VX_config_reg_if #(
-    .NUM(NUM_REGS),
-    .DW(REGS_DW)
-  ) config_reg_if();
-
-  VX_node_done_if node_done_if();
-
-  initial begin
-    if (config_reg_if.DW != REGS_DW) $fatal(1, "config_reg_if.DW must be 32");
-    if (config_reg_if.NUM < NUM_REGS) $fatal(1, "config_reg_if.NUM(%0d) < NUM_REGS(%0d)", config_reg_if.NUM, NUM_REGS);
-  end
-
-
   VX_gemm_dma_ctrl #(
+    .DMA_CFG_BASE_ADDR(`DMA_REG_BASE_ADDR),
     .DMA_CFG_STRIDE_BYTES(REGS_DW/8),
     .DMA_ENTRY_STRIDE_BYTES(NUM_REGS*REGS_DW/8),
     .ENTRYID_W(ENTRYID_W),
@@ -58,27 +46,17 @@ module VX_gemm_dma_ctrl_with_dma import VX_gpu_pkg::*; #(
     .dma_if(dma_if[0].master)
   );
 
-  VX_job_frontend #(
-    .NUM_ENTRIES(NUM_ENTRIES),
-    .NUM_REGS32(NUM_REGS),
-    .ENTRYID_W(ENTRYID_W)
-  ) config_register (
+  VX_dma_node #(
+    .INSTANCE_ID(INSTANCE_ID),
+    .N_MASTER(1),
+    .NUM_ENTRIES(NUM_ENTRIES)
+  ) dma_node (
     .clk(clk),
     .reset(reset),
 
-    .mmio_if(dma_if),
-    .issue_if(config_reg_if.master),
-    .done_if(node_done_if.slave)
-  );
-
-  VX_dma_unit_misal #(.INSTANCE_ID("dma0")) dma_node (
-    .clk          (clk),
-    .reset        (reset),
-    .cfg_reg_if   (config_reg_if.slave),
+    .mmio_if(dma_if.slave),
     .dcache_bus_if(dcache_bus_if),
-    .lmem_bus_if  (lmem_bus_if),
-    .done_if      (node_done_if.master)
+    .lmem_bus_if(lmem_bus_if)
   );
-
 
 endmodule
