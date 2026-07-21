c45fc5b3c3fafe866ac305c91fd6fd6e26f00012
Jaeyong Jang <jong1664@gmail.com>
2026-07-21 23:02:46 +0900

    update suites to use thread 32 case

--- analysis_workspace/latency_on_hw/make_case.sh
+++ analysis_workspace/latency_on_hw/make_case.sh
@@ -10,31 +10,31 @@ if [[ "$target" == "full" ]]; then
     --input suites/llama2_7b \
     --output generated_suites/llama2_7b_main_full \
     --prefill-batches 1 \
-    --prefill-seq-lens 1024,2048,4096,8192,16384,32768,65536 \
+    --prefill-seq-lens 1024,2048,4096,8192,16384,32768 \
     --generation-batches 1,2,4 \
-    --generation-seq-lens 1024,2048,4096,8192,16384,32768,65536
+    --generation-seq-lens 1024,2048,4096,8192,16384,32768
 
   ./make_cases.sh \
     --input suites/llama3_8b \
     --output generated_suites/llama3_8b_main_full \
     --prefill-batches 1 \
-    --prefill-seq-lens 1024,2048,4096,8192,16384,32768,65536 \
+    --prefill-seq-lens 1024,2048,4096,8192,16384,32768 \
     --generation-batches 1,2,4 \
-    --generation-seq-lens 1024,2048,4096,8192,16384,32768,65536
+    --generation-seq-lens 1024,2048,4096,8192,16384,32768
 else
   ./make_cases.sh \
     --input suites/llama2_7b \
     --output generated_suites/llama2_7b_main \
     --prefill-batches 1 \
-    --prefill-seq-lens 65536 \
+    --prefill-seq-lens 1024,2048,4096,8192,16384,32768 \
     --generation-batches 1,2,4 \
-    --generation-seq-lens 65536
+    --generation-seq-lens 1024,2048,4096,8192,16384,32768
 
   ./make_cases.sh \
     --input suites/llama3_8b \
     --output generated_suites/llama3_8b_main \
     --prefill-batches 1 \
-    --prefill-seq-lens 65536 \
+    --prefill-seq-lens 1024,2048,4096,8192,16384,32768 \
     --generation-batches 1,2,4 \
-    --generation-seq-lens 65536
+    --generation-seq-lens 1024,2048,4096,8192,16384,32768
 fi
