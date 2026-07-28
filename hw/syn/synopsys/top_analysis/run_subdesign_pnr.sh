0c671a57b0e4209f088477a3c92aa302b9d417f9
jaeyongjang@mustang <jong166@gmail.com>
2026-07-29 01:19:25 +0900

    update C3 subdesign spec

--- hw/syn/synopsys/top_analysis/run_subdesign_pnr.sh
+++ hw/syn/synopsys/top_analysis/run_subdesign_pnr.sh
@@ -15,3 +15,8 @@ python hw/syn/synopsys/top_analysis/run_subdesign_pnr.py \
     --config configs/tcu_th32_c1_rev2.sh \
     --candidate-config hw/syn/synopsys/top_analysis/C1_subdesign_partial.yaml \
     --run-dir build/hw/syn/synopsys/top_analysis/Vortex_tcu_th32_c1_rev2_subdesign_partial
+
+python hw/syn/synopsys/top_analysis/run_subdesign_pnr.py \
+    --config configs/naive_gemm_th32_tcol32_hwexp_dcache_sxbar_f16.sh \
+    --candidate-config hw/syn/synopsys/top_analysis/C3_subdesign_partial.yaml \
+    --run-dir build/hw/syn/synopsys/top_analysis/Vortex_naive_gemm_th32_tcol32_hwexp_dcache_sxbar_f16_subdesign_partial \
