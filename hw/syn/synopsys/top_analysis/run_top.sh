cfa04dd35a0ae43187127b5a486d56e0fa8837b4
jaeyongjang@mustang <jong166@gmail.com>
2026-07-30 16:53:05 +0900

    Add sub design reports

--- hw/syn/synopsys/top_analysis/run_top.sh
+++ hw/syn/synopsys/top_analysis/run_top.sh
@@ -2,3 +2,5 @@
 
 python hw/syn/synopsys/top_analysis/run.py --config configs/naive_gemm_th32_tcol32_hwexp_dcache_sxbar_f16.sh
 python hw/syn/synopsys/top_analysis/run.py --config configs/improve_th32_tcol32_hwexp_dcache_sxbar_f16.sh
+python hw/syn/synopsys/top_analysis/run.py --config configs/improve_th32_tcol32_hwexp_dcache_sxbar_f16.sh --run-dir \
+/home/jaeyong.jang/project.local/research/vortex/build/hw/syn/synopsys/top_analysis/Vortex_axi_improve_th32_tcol32_hwexp_dcache_sxbar_f16_acc_fix
