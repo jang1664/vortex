704c3f1a875deb3e278ddb86ca0716d8b6e2c58a
jaeyongjang@ferrari <jong1664@gmail.com>
2026-07-30 16:40:08 +0900

    use HS banks for compiled single port sram

--- hw/syn/synopsys/top_analysis/run_top.sh
+++ hw/syn/synopsys/top_analysis/run_top.sh
@@ -2,3 +2,4 @@
 
 python hw/syn/synopsys/top_analysis/run.py --config configs/naive_gemm_th32_tcol32_hwexp_dcache_sxbar_f16.sh
 python hw/syn/synopsys/top_analysis/run.py --config configs/improve_th32_tcol32_hwexp_dcache_sxbar_f16.sh
+python hw/syn/synopsys/top_analysis/run.py --config configs/tcu_th32_f16.sh
